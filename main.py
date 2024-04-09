43cb52b9ace6a9cf14d80d35631e18cb86f0870b
Bruno-Otavio <brunotavio.contatoescola@gmail.com>
2024-04-09 09:49:23 -0300

    Fixed the bot

--- main.py
+++ main.py
@@ -1,102 +1,196 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchFrameException
 
 from time import sleep
 from datetime import timedelta
+from argparse import ArgumentParser, Namespace
+
+
+def debug(mtype, message):
+	if args.verbose:
+		if mtype == 'error':
+			print(f"\032[ERROR]\033 {message}")
+		if mtype == 'info':
+			print(f"\032[INFO]\033 {message}")
+		elif mtype == 'warning':
+			print(f"\032[WARNING]\033 {message}")
+		elif mtype == 'success':
+			print(f"\032[SUCCESS]\033 {message}")
+		else:
+			print(f"\032[DEBUG]\033 {message}")
+
+
+parser = ArgumentParser()
+
+parser_login = parser.add_argument_group('Login')
+
+parser_login.add_argument('-u', '--ra', type=str, help='RA of the student, with digit')
+parser_login.add_argument('-d', '--uf', type=str, help='UF of the sudent', default='sp')
+parser_login.add_argument('-p', '--password', type=str, help='Password of the student')
+parser_login.add_argument('-c', '--channel', type=str, help='Channel of choice', choices=['3c', 'c3'])
+parser_login.add_argument('-a', '--amount', type=int, help='Amount of videos to watch', default=0)
+
+parser_debug = parser.add_argument_group('Debug')
+
+parser_debug.add_argument('-v', '--verbose', help='Verbose mode', action='store_false', default=True)
+
+args: Namespace = parser.parse_args()
+
+if args.ra and args.password and args.channel:
+	arg_ra = args.ra
+	arg_uf = args.uf
+	arg_password = args.password
+	arg_channel = args.channel
+	arg_amount = args.amount
+else:
+	debug('warning', 'Not all arguments were informed')
+	exit()
+
 
 driver = webdriver.Chrome()
+driver.get('https://cmspweb.ip.tv/login')
 
-driver.get("https://cmspweb.ip.tv")
-
-wait = WebDriverWait(driver, timeout=2)
-implicit_wait = driver.implicitly_wait(1000 * 5)
-
-user_ra = ""
-user_password = ""
-
-records_class = "3C" # Terá a opção para o 3C e C3
+implicit_wait = driver.implicitly_wait(5)
+wait = WebDriverWait(driver, 900)
 
 
-def login():
-    wait.until(lambda driver : driver.find_element(By.ID, "access-student")).click()
+def login(ra, uf, password):
+	debug('info', f'Logging in with RA:{ra} and UF:{uf}')
 
-    wait.until(lambda driver : driver.find_element(By.ID, "ra-student")).send_keys(user_ra[slice(0, -1)])
-    wait.until(lambda driver : driver.find_element(By.ID, "digit-student")).send_keys(user_ra[slice(-1, -2, -1)])
+	wait.until(lambda driver: driver.find_element(By.ID, 'access-student')).click()
 
-    wait.until(lambda driver : driver.find_element(By.ID, "password-student")).send_keys(user_password)
+	wait.until(lambda driver: driver.find_element(By.ID, 'ra-student')).send_keys(ra[slice(0, -1)])
+	wait.until(lambda driver: driver.find_element(By.ID, 'digit-student')).send_keys(ra[slice(-1, -2, -1)])
 
-    wait.until(lambda driver : driver.find_element(By.ID, "btn-login-student")).click()
+	uf_student = wait.until(lambda driver: driver.find_element(By.ID, 'uf-student'))
+	Select(uf_student).select_by_value(uf)
 
-    enter_records()
+	wait.until(lambda driver: driver.find_element(By.ID, 'password-student')).send_keys(password)
+
+	wait.until(lambda driver: driver.find_element(By.ID, 'btn-login-student')).click()
+
+	debug('success', f'Logged in with RA:{ra} and UF:{uf}')
 
 
-def enter_records():
-    if records_class == "3C":
-        wait.until(lambda driver : driver.find_element(By.ID, "lproom_r3b3556ff22951b448-l")).click()
-    elif records_class == "C3":
-        wait.until(lambda driver : driver.find_element(By.ID, "lproom_r43d55cb67a22a9f47-l")).click()
+def navigate(channel):
+	debug('info', 'Entering Recordings Room')
 
-    wait.until(lambda driver : driver.find_element(By.ID, "roomDetailCsm")).click()
+	implicit_wait
+	if channel.lower() == '3c':
+		wait.until(lambda driver: driver.find_element(By.ID, 'lproom_r3b3556ff22951b448-l')).click()
+	elif channel.lower() == 'c3':
+		wait.until(lambda driver: driver.find_element(By.ID, 'lproom_r43d55cb67a22a9f47-l')).click()
 
-    driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
-
-    watch_videos()
+	implicit_wait
+	wait.until(lambda driver: driver.find_element(By.ID, 'roomDetailCsm')).click()
+	
+	debug('success', 'Entered Recordings Room')
 
 
 def count_videos():
-    videos = wait.until(lambda driver : driver.find_elements(By.CLASS_NAME, "MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.MuiCard-root.css-1154o8t"))
-    return videos, int(len(videos))
+	debug('info', 'Counting videos')
+
+	driver.switch_to.default_content()
+
+	try:
+		implicit_wait
+		iframe = wait.until(lambda driver: driver.find_element(By.XPATH, '//*[@id="recordings-area-body"]/iframe'))
+		driver.switch_to.frame(iframe)
+	except NoSuchFrameException:
+		debug('error', 'Failed to switch to iframe')
+		exit()
+
+	videos_list = wait.until(lambda driver: driver.find_elements(By.CLASS_NAME, 'MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation1.MuiCard-root.css-1154o8t'))
+
+	driver.switch_to.default_content()
+
+	video_amount = int(len(videos_list))
+
+	debug('success', f'Found {video_amount} videos')
+
+	return videos_list, video_amount
 
 
-def video_countdown(time, offset=0):
-    video_duration = int(time[0])*3600 + int(time[1])*60 + int(time[2]) + offset
-
-    while video_duration > 0:
-        timer = timedelta(seconds=video_duration)
-        print(timer, end="\r")
-
-        sleep(1)
-        video_duration -= 1
-
-
-def play_video():
-    action = ActionChains(driver)
-    action.send_keys(Keys.TAB).perform()
-    action.send_keys(Keys.SPACE).perform()
+def video_countdown(time):
+	video_duration = time
+	while video_duration > 0:
+		timer = timedelta(seconds = video_duration)
+		sleep(1)
+		video_duration -= 1
+		print(f'Remaining Time: {timer}', end='\r')
 
 
 def mute_video():
-    action = ActionChains(driver)
-    for i in range(0, 20):
-        action.send_keys(Keys.ARROW_DOWN).perform()
+	action = ActionChains(driver)
+	for i in range(0, 20):
+		action.send_keys(Keys.DOWN).perform()
 
 
-def watch_videos():
-    videos = count_videos()
-    videos_list = videos[0]
-    videos_amount = videos[1]
+def watch_videos(amount):
+	videos = count_videos()
+	videos_list = videos[0]
+	videos_amount = videos[1]
+	video_num = 0
+	watched = 0
 
-    action = ActionChains(driver)
+	video_duration = 0
+	offset_time = 15
 
-    for video in videos_list:
-        print(f"Videos Left: {videos_amount}", end="\r")
+	action = ActionChains(driver)
 
-        time = wait.until(lambda video : video.find_elements(By.CLASS_NAME, "MuiTypography-root.MuiTypography-caption.MuiTypography-noWrap.css-guitlj"))[5].text
-        time = time.split(" ", 1)[1].split(":")
+	if amount == 0 or amount > videos_amount:
+		watch_amount = videos_amount
+		display_amount = videos_amount
+	else:
+		watch_amount = amount
+		display_amount = amount
 
-        wait.until(lambda video : video.find_element(By.CLASS_NAME, "react-player__preview")).click()
+	debug('info', f'Watching {watch_amount} videos')
 
-        play_video()
-        mute_video()
-        video_countdown(time)
+	try:
+		implicit_wait
+		iframe = wait.until(lambda driver: driver.find_element(By.TAG_NAME, 'iframe'))
+		driver.switch_to.frame(iframe)
+	except NoSuchFrameException:
+		debug('error', 'Failed to switch to iframe')
+		exit()
 
-        action.send_keys(Keys.ESCAPE).perform() # Exit video
+	while watch_amount > 0:
+		debug('info', f'Watching video {video_num+1}/{display_amount}')
 
-        videos_amount -= 1
+		implicit_wait
+		time = wait.until(lambda driver: videos_list[video_num].find_elements(By.CLASS_NAME, 'MuiTypography-root.MuiTypography-caption.MuiTypography-noWrap.css-guitlj'))[5].text
+		time = time.split(' ')[1].split(':')
+		video_duration = int(time[0])*3600 + int(time[1])*60 + int(time[2]) # Hours, Minutes, Seconds
+
+		video = videos_list[video_num]
+		wait.until(lambda driver: video.find_element(By.CLASS_NAME, 'react-player__preview')).click()
+		
+		action.send_keys(Keys.TAB).perform()
+		mute_video()
+		action.send_keys(Keys.SPACE).perform()
+
+		implicit_wait
+		video_countdown(video_duration)
+
+		action.send_keys(Keys.ESCAPE).perform()
+
+		video_num += 1
+		watched += 1
+		watch_amount -= 1
+
+	driver.switch_to.default_content()
+
+	debug('success', f'Finished {video_num}/{display_amount} videos')
 
 
-login()
-driver.close()
+login(arg_ra, arg_uf, arg_password)
+navigate(arg_channel)
+watch_videos(arg_amount)
+
+driver.quit()
---
+++ readme.txt
@@ -0,0 +1,19 @@
+atenção: é necessário ter o python instalado 
+
+antes de iniciar o programa abra o cmd dentro desta pasta e digite o seguinte:
+
+py -m pip install -r requirements.txt
+
+para rodar o bot abra o cmd na pasta novamente e digite o seguinte no console:
+
+py main.py -u [ra com digito sem o SP] -p [senha] -c [canal dos vídeos, sem ou anual]
+
+você também pode usar argumentos como: 
+
+-u [escolhe o uf do ra, por padrão é SP] -a [quantidade de vídeos que você quer que o bot veja, por padrão ele vê todos]
+
+obs: não é para escrever com os []
+
+caso você queira uma explicação do que cada argumento faz digite:
+
+py main.py --help
